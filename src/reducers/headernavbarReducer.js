21567c115b3d4319c96679ca697bec8688610d98
may2018 <ramos.jackie233@gmail.com>
2020-11-20 17:08:54 -0700

    refactored navbar to take additional onclick

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,13 +1,13 @@
 import {
-    CHANGE_NAVBAR_ACTIVE,
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from '../actions/types';
 
 const INITIAL_STATE = {
     headerLinks: [],
-    navbarLinks: []
-       
+    navbarLinks: [],
+    onClick: ''
 }
 
 export default function(state = INITIAL_STATE, action) {
@@ -18,24 +18,24 @@ export default function(state = INITIAL_STATE, action) {
                 headerLinks: action.payload
             }
         case SET_NAVBAR_LINKS:
+        const { links, onClick } = action.payload; 
             return {
                 ...state,
-                navbarLinks: action.payload
+                navbarLinks: links,
+                onClick: onClick
             }
-        case CHANGE_NAVBAR_ACTIVE:
-            console.log(state.navbarLinks);
+        case CHANGE_NAVBAR_ACTIVE: 
             const navbarLinks = state.navbarLinks.map(link => {
                 link.active = false;
                 if(link._id == action.payload) {
                     link.active = true;
                 }
                 return link;
-                })
-                return {
-                    ...state,
-                    navbarLinks
-                }
-            
+            })
+            return {
+                ...state,
+                navbarLinks
+            }
         default: return state;
     }
 }
